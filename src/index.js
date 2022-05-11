67efa884c42c50fe3b65a523aba5e706af874daf
Aleksandr Raskulov <alexandr1991r@gmail.com>
2022-05-11 22:20:40 +0300

    minor update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import './sass/main.scss';
 // Slider
 $('.slider').slick({
-    arrows: true,
-    dots: true,
-    infinite: true,
-    fade: true,
-    cssEase: 'linear',
-    autoplay:true,
+  arrows: true,
+  dots: true,
+  infinite: true,
+  fade: true,
+  cssEase: 'linear',
+  autoplay: true,
 });
